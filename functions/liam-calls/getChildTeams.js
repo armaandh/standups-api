7c9e7a1121b46a6379f26a5148b4b245ca6e177c
armaandh <dhanjiarmaan@gmail.com>
2018-05-04 14:55:04 -0700

    changes to files

--- functions/liam-calls/getChildTeams.js
+++ functions/liam-calls/getChildTeams.js
@@ -1,23 +1,23 @@
 var AWS = require('aws-sdk');
 var uuid = require('uuid');
-AWS.config.update({region: 'us-east-2'});
+AWS.config.update({ region: 'us-east-2' });
 
 
-module.exports.getChildTeams = (event, context, callback) => {
+module.exports.handler = (event, context, callback) => {
     // TODO implement
     var dynamodb = new AWS.DynamoDB();
-    
- var params = {
-  ExpressionAttributeValues: {
-   ":v1": {
-     S: event.teamid
-    }
-  }, 
-  KeyConditionExpression: "ParentID = :v1",
-  TableName: "Teams"
- };
- dynamodb.query(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     callback(null, data);           // successful response
- });
+
+    var params = {
+        ExpressionAttributeValues: {
+            ":v1": {
+                S: event.teamid
+            }
+        },
+        KeyConditionExpression: "ParentID = :v1",
+        TableName: "TeamsTbl"
+    };
+    dynamodb.query(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else callback(null, data); // successful response
+    });
 };
