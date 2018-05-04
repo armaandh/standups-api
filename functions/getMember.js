d8d1dfc36539160ca4322988ba5aed6b8da13ebc
armaandh <dhanjiarmaan@gmail.com>
2018-05-04 13:19:26 -0700

    updates to dynamodb structure

---
+++ functions/getMember.js
@@ -0,0 +1,38 @@
+/**
+ * GET /members
+ *
+ * Returns a collection of ALL members.
+ * @returns {Array.Object}
+ */
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = (event, context, callback) => {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            id: event.pathParameters.id,
+        },
+    };
+
+    dynamoDb.get(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Couldn\'t fetch the todo item.',
+            });
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+        };
+        callback(null, response);
+    });
+};
--- functions/getMembers.js
+++ functions/getMembers.js
@@ -4,24 +4,38 @@
  * Returns a collection of ALL members.
  * @returns {Array.Object}
  */
-module.exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        },
-        body: JSON.stringify({
-            members: [{
-                id: 2,
-                name: 'James',
-                address: 'james@mail.com',
-            }, {
-                id: 3,
-                name: 'Jimmy',
-                address: 'jimmy@mail.com',
-            }, ],
-        }),
-    };
 
-    callback(null, response);
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+};
+
+module.exports.handler = (event, context, callback) => {
+    dynamoDb.scan(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' },
+                body: 'Couldn\'t fetch the todos.',
+            });
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Items),
+            headers: {
+                "X-Requested-With": '*',
+                "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                "Access-Control-Allow-Origin": '*',
+                "Access-Control-Allow-Methods": 'GET,OPTIONS'
+            }
+        };
+        callback(null, response);
+    });
+
 };
