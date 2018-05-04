70b3627253113d1b356550d7107fe448bfc4afcd
Liam <32491405+DakKhuza@users.noreply.github.com>
2018-05-04 16:58:47 -0700

    "Fixed" all team methods

--- functions/liam-calls/createTeam.js
+++ functions/liam-calls/createTeam.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 var uuid = require('uuid');
-AWS.config.update({ region: 'us-east-2' });
+AWS.config.update({ region: 'us-east-1' });
 
 
 module.exports.handler = (event, context, callback) => {
@@ -8,16 +8,24 @@ module.exports.handler = (event, context, callback) => {
     var dynamodb = new AWS.DynamoDB();
 
     var params = {
-        TableName: 'TeamsTbl',
+        TableName: 'Teams',
         Item: {
-            'ParentID': { S: event.parentid },
+            'ParentID': { S: JSON.parse(event.body).parentid },
             'ID': { S: `${uuid.v1()}` },
-            'Name': { S: event.name }
+            'Name': { S: JSON.parse(event.body).name }
         }
     };
 
     // Call DynamoDB to add the item to the table
     dynamodb.putItem(params, function(err, data) {
+        var response = {
+            "statusCode": 200,
+            "headers": {
+                "my_header": "my_value"
+            },
+            "body": JSON.stringify(data),
+            "isBase64Encoded": false
+        };
         if (err) {
             callback(null, err);
         } else {
--- functions/liam-calls/createTeamMember.js
+++ functions/liam-calls/createTeamMember.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 var uuid = require('uuid');
-AWS.config.update({ region: 'us-east-2' });
+AWS.config.update({ region: 'us-east-1' });
 
 
 module.exports.handler = (event, context, callback) => {
@@ -8,17 +8,25 @@ module.exports.handler = (event, context, callback) => {
     var dynamodb = new AWS.DynamoDB();
 
     var params = {
-        TableName: 'TeamMembersTbl',
+        TableName: 'TeamMembers',
         Item: {
-            'TeamID': { S: event.teamid },
+            'TeamID': { S: JSON.parse(event.body).teamid },
             'ID': { S: `${uuid.v1()}` },
-            'Name': { S: event.name },
+            'Name': { S: JSON.parse(event.body).name },
             'UserpoolID': { S: 'NA' }
         }
     };
 
     // Call DynamoDB to add the item to the table
     dynamodb.putItem(params, function(err, data) {
+        var response = {
+            "statusCode": 200,
+            "headers": {
+                "my_header": "my_value"
+            },
+            "body": JSON.stringify(data),
+            "isBase64Encoded": false
+        };
         if (err) {
             callback(null, err);
         } else {
