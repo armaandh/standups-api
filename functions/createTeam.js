3fc2d5b682e7e25f749fe2c344ae960a4530f621
Liam <32491405+DakKhuza@users.noreply.github.com>
2018-05-08 13:11:42 -0700

    Moved new functions to functions folder

---
+++ functions/createTeam.js
@@ -0,0 +1,35 @@
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+AWS.config.update({ region: 'us-east-1' });
+
+
+module.exports.handler = (event, context, callback) => {
+    // TODO implement
+    var dynamodb = new AWS.DynamoDB();
+
+    var params = {
+        TableName: 'TeamsTbl',
+        Item: {
+            'ParentID': { S: JSON.parse(event.body).parentid },
+            'ID': { S: `${uuid.v1()}` },
+            'Name': { S: JSON.parse(event.body).name }
+        }
+    };
+
+    // Call DynamoDB to add the item to the table
+    dynamodb.putItem(params, function(err, data) {
+        var response = {
+            "statusCode": 200,
+            "headers": {
+                "my_header": "my_value"
+            },
+            "body": JSON.stringify(data),
+            "isBase64Encoded": false
+        };
+        if (err) {
+            callback(null, err);
+        } else {
+            callback(null, response);
+        }
+    });
+};
---
+++ functions/getTeamMembers.js
@@ -0,0 +1,31 @@
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+AWS.config.update({ region: 'us-east-1' });
+
+
+module.exports.handler = (event, context, callback) => {
+    // TODO implement
+    var dynamodb = new AWS.DynamoDB();
+
+    var params = {
+        ExpressionAttributeValues: {
+            ":v1": {
+                S: JSON.parse(event.body).teamid
+            }
+        },
+        KeyConditionExpression: "TeamID  = :v1",
+        TableName: "TeamMembersTbl"
+    };
+    dynamodb.query(params, function(err, data) {
+        var response = {
+            "statusCode": 200,
+            "headers": {
+                "my_header": "my_value"
+            },
+            "body": JSON.stringify(data),
+            "isBase64Encoded": false
+        };
+        if (err) console.log(err, err.stack); // an error occurred
+        else callback(null, response); // successful response
+    });
+};
